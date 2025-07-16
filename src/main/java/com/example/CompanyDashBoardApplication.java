63a77b6d188a58e3164e1a696e10bbfb5ab953f3
prakash-kadali <prakashkadali20@gmail.com>
2025-07-16 17:37:07 +0530

    war commit

--- src/main/java/com/example/CompanyDashBoardApplication.java
+++ src/main/java/com/example/CompanyDashBoardApplication.java
@@ -2,24 +2,34 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 import com.example.config.AuthFilter;
 
-@SpringBootApplication
-public class CompanyDashBoardApplication {
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+public class CompanyDashBoardApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CompanyDashBoardApplication.class, args);
-	}
-	@Bean
-	public FilterRegistrationBean<AuthFilter> loggingFilter(){
-	    FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
-	    registrationBean.setFilter(new AuthFilter());
-	    registrationBean.addUrlPatterns("/admin-dashboard", "/emp-dashboard");
-	    return registrationBean;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyDashBoardApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CompanyDashBoardApplication.class);
+    }
 
+    @Bean
+    public FilterRegistrationBean<AuthFilter> loggingFilter() {
+        FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AuthFilter());
+        registrationBean.addUrlPatterns("/admin-dashboard", "/emp-dashboard");
+        return registrationBean;
+    }
+
+   
 }
--- src/main/java/com/example/config/AuthFilter.java
+++ src/main/java/com/example/config/AuthFilter.java
@@ -1,14 +1,12 @@
 package com.example.config;
 
 import jakarta.servlet.*;
-import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/admin-dashboard", "/emp-dashboard"}) // secure only these URLs
 public class AuthFilter implements Filter {
 
     @Override
